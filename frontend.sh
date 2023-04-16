a6b9aec373dd019c83de641be109f6d2cc27fb90
pavanbairu <pavan.bairu1@gmail.com>
2023-04-16 21:52:18 +0530

    created the frontend.sh file

---
+++ frontend.sh
@@ -0,0 +1,18 @@
+yum install nginx -y
+systemctl enable nginx
+systemctl start nginx
+
+# created the roboshop config file
+cp roboshop.conf cd /etc/nginx/default.d/roboshop.conf
+
+#removing the existing content available in the below location
+rm -rf /usr/share/nginx/html/*
+curl -L -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip
+cd /usr/share/nginx/html/
+unzip /tmp/frontend.zip
+
+systemctl restart nginx
+
+
+
+
