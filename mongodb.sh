95ed66211b5c38db43423debc8ca8c63d01bf450
pavanbairu <pavan.bairu1@gmail.com>
2023-04-18 11:45:24 +0530

    updated the listening address of mongodb

--- mongodb.sh
+++ mongodb.sh
@@ -7,8 +7,8 @@ yum install mongodb-org -y
 
 # need to replace the ip address 127.0.0.1 with 0.0.0.0 in cd /etc/mongo.conf location
 echo -e "\e[36m need to change the mongodb listening address\e[0m"
-sed -i -e 's|127.0.0.0|0.0.0.0|' /etc/mongod.conf
+sed -i -e 's|127.0.0.1|0.0.0.0|' /etc/mongod.conf
 
-echo -e "\e [36m start mongodb \e[0m"
+echo -e "\e[36m start mongodb \e[0m"
 systemctl enable mongod
 systemctl restart mongod
