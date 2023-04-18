728756aa202b3cc99e6b789a5c9a2e9cfeba9655
pavanbairu <pavan.bairu1@gmail.com>
2023-04-18 12:52:29 +0530

    created rabbitmq.sh

---
+++ rabbitmq.sh
@@ -0,0 +1,25 @@
+# set up repo
+echo -e "\e[36m setup erlang repo\e[0m"
+curl -sL https://packagecloud.io/install/repositories/rabbitmq/erlang/script.rpm.sh | bash
+
+# Install ErLang
+echo -e "\e[36m Install ErLang\e[0m"
+yum install erlang -y
+
+# setup the rabbitmq repo
+echo -e "\e[36m setup the rabbitmq repo \e[0m"
+curl -sL https://packagecloud.io/install/repositories/rabbitmq/rabbitmq-server/script.rpm.sh | bash
+
+# install rabbitmq server
+echo -e "\e[36m install rabbitmq server \e[0m"
+yum install rabbitmq-server -y
+
+# start the service
+echo -e "\e[36m start the service\e[0m"
+systemctl enable rabbitmq
+systemctl restart rabbitmq
+
+# rabbitmq comes with default username and password guest:guest. lets change it
+echo -e "\e[36m change default username and password\e[0m"
+rabbitmqctl add_user roboshop roboshop123
+rabbitmqctl set_permissions -p / roboshop ".*" ".*" ".*"
