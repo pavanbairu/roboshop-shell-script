5fba5d6c4622e2eb66196444a6b9165fa91a009b
pavanbairu <pavan.bairu1@gmail.com>
2023-04-20 11:24:08 +0530

    added conditions for shipping.sh 1. keep the the common code in common.sh 2. added status check function to know the status of each command execution output and terminating if any stage fails

--- common.sh
+++ common.sh
@@ -1,69 +1,130 @@
 app_user=roboshop
 
-print(){
+func_print(){
   echo -e "\e[36m $1 \e[0m"
 }
 
-
-
-schema_setup() {
+func_status_check()
+{
+  if [ $1 -eq 0 ]; then
+    echo "\e[32m SUCCESS \e[0m"
+  else
+    echo "\e[31m FAILURE \e[0m"
+    exit
+  fi
+}
+func_schema_setup() {
 
    if [ "$schema_setup" == "mongo" ]; then
     # now we need to load schema to function the mongodb
     # created the mongo repo file
-    print "setup mongo repo file and copying it"
+    func_print "setup mongo repo file and copying it"
     cp ${script_path}/mongo.repo /etc/yum.repos.d/mongo.repo
+    func_status_check $? #to checck the status of previous command or stage
 
     # install mongodb client
-    print "install mongodb client"
+    func_print "install mongodb client"
     yum install mongodb-org-shell -y
+    func_status_check $? #to checck the status of previous command or stage
 
     # loading the schema
-    print "loading the schema"
+    func_print "loading the schema"
     mongo --host mongodb-dev.pavanbairu.tech </app/schema/catalogue.js
+    func_status_check $? #to checck the status of previous command or stage
+  fi
+  if [ "$schema_setup" == "mysql" ]; then
+
+      func_print "setup mqsql repo file and copying it"
+      cp ${script_path}/mysql.repo /etc/yum.repos.d/mysql.repo
+      func_status_check $? #to checck the status of previous command or stage
+
+      func_print "install the mysql client"
+      yum install mysql -y
+      func_status_check $? #to checck the status of previous command or stage
+
+      func_print "load schema"
+      mysql -h mysql-dev.pavanbairu.tech -uroot -p${mysql_root_password} < /app/schema/${component}.sql
+      func_status_check $? #to checck the status of previous command or stage
   fi
 }
+
+func_app_prereq(){
+
+  # add application user
+    func_print "add application user"
+    useradd ${app_user}
+    func_status_check $? #to checck the status of previous command or stage
+
+    func_print "create directory"
+    rm -rf /app
+    mkdir /app
+    func_status_check $? #to checck the status of previous command or stage
+
+    # download the application code
+    func_print "download the application code"
+    curl -L -o /tmp/${component}.zip https://roboshop-artifacts.s3.amazonaws.com/${component}.zip
+    cd /app
+    unzip /tmp/${component}.zip
+    func_status_check $? #to checck the status of previous command or stage
+
+}
+
+func_systemd_setup(){
+
+  # created the service and copying it
+    func_print "created the ${component} service and copying it"
+    cp ${script_path}/${component}.service /etc/systemd/system/${component}.service
+    func_status_check $? #to checck the status of previous command or stage
+
+    # start the service
+    func_print "start the ${component} service"
+    systemctl daemon-reload
+    systemctl enable ${component}
+    systemctl restart ${component}
+    func_status_check $? #to checck the status of previous command or stage
+
+}
+
 func_nodejs(){
 
   # setup node js repo
-  print "setup nodejs"
+  func_print "setup nodejs"
   curl -sL https://rpm.nodesource.com/setup_lts.x | bash
+  func_status_check $? #to checck the status of previous command or stage
 
   # install nodejs
-  print "install nodejs"
+  func_print "install nodejs"
   yum install nodejs -y
+  func_status_check $? #to checck the status of previous command or stage
 
-  # add application user
-  print "add application user"
-  useradd ${app_user}
-
-  print "create directory"
-  rm -rf /app
-  mkdir /app
-
-  # download the application code
-  print "download the application code"
-  curl -L -o /tmp/${component}.zip https://roboshop-artifacts.s3.amazonaws.com/${component}.zip
-  cd /app
-  unzip /tmp/${component}.zip
+  func_app_prereq # calling app user
 
   # download the dependencies
-  print "download the dependencies"
+  func_print "download the dependencies"
   npm install
+  func_status_check $? #to checck the status of previous command or stage
 
-  # created the cart service and copying it
-  print "created the ${component} service and copying it"
-  cp ${script_path}/${component}.service /etc/systemd/system/${component}.service
+  func_schema_setup
+  func_systemd_setup
 
-  #Load the service.
-  print "Load the service"
-  systemctl daemon-reload
+}
 
-  # start the service
-  print "start the ${component} service"
-  systemctl enable ${component}
-  systemctl restart ${component}
+func_java(){
+  
+  # need to install the java and it comes within maven package
+  func_print "install maven"
+  yum install maven -y
+  func_status_check $? #to checck the status of previous command or stage
+  
+  func_app_prereq # calling app user
+  
+  # download the dependencies and build application
+  func_print "download the dependencies and build application"
+  mvn clean package
+  mv target/${component}-1.0.jar ${component}.jar
+  func_status_check $? #to checck the status of previous command or stage
 
-  schema_setup
-
+  func_schema_setup
+  func_systemd_setup
+  
 }
--- shipping.sh
+++ shipping.sh
@@ -9,56 +9,6 @@ if [ -z "${mysql_root_password}" ]; then
   exit
 fi
 
-# need to install the java and it comes within maven package
-echo -e "\e[36m install maven \e[0m"
-yum install maven -y
-
-#add application user
-echo -e "\e[36m add application user \e[0m"
-useradd ${app_user}
-
-# create the new directory
-echo -e "\e[36m create the new directory \e[0m"
-rm -rf /app
-mkdir /app
-
-# download the application code
-echo -e "\e[36m download the application code \e[0m"
-curl -L -o /tmp/shipping.zip https://roboshop-artifacts.s3.amazonaws.com/shipping.zip
-cd /app
-unzip /tmp/shipping.zip
-
-# download the dependencies and build application
-echo -e "\e[36m download the dependencies and build application \e[0m"
-mvn clean package
-mv target/shipping-1.0.jar shipping.jar
-
-# created the shipping service and copying it
-echo -e "\e[36m setup the shipping service and copying it \e[0m"
-cp ${script_path}/shipping.service /etc/systemd/system/shipping.service
-
-# Load the service.
-echo -e "\e[36m Load the service \e[0m"
-systemctl daemon-reload
-
-# start the service
-echo -e "\e[36m start the service \e[0m"
-systemctl enable shipping
-systemctl restart shipping
-
-# need to load the mqsql schema to function the db
-# setup mqsql repo file and copying it
-echo -e "\e[36m setup mqsql repo file and copying it \e[0m"
-cp ${script_path}/mysql.repo /etc/yum.repos.d/mysql.repo
-
-# install the mysql client
-echo -e "\e[36m install the mysql client \e[0m"
-yum install mysql -y
-
-# load schema
-echo -e "\e[36m load schema \e[0m"
-mysql -h mysql-dev.pavanbairu.tech -uroot -p${mysql_root_password} < /app/schema/shipping.sql
-
-# restart the service
-echo -e "\e[36m restart the service \e[0m"
-systemctl restart shipping
+component=shipping
+schema_setup=mysql
+func_java
