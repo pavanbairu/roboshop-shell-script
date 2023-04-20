67ee0db91c4cd40ca70ff3a59e17f9f204b70ab5
pavanbairu <pavan.bairu1@gmail.com>
2023-04-20 18:00:56 +0530

    updated the script path

--- rabbitmq.sh
+++ rabbitmq.sh
@@ -1,3 +1,6 @@
+script=$(realpath "$0") # gives current file path including filename
+script_path=$(dirname "$script") # gives the current file directory
+
 source ${script_path}/common.sh
 rabbitmq_appuser_password=$1
 
